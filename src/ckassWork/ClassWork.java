a4e46d34aab3e95eaade23c546093cdc805b7f01
IrynaMamchur <puhovka2014@ukr.net>
2022-10-11 02:10:01 +0300

    Add homelesson5

---
+++ src/ckassWork/ClassWork.java
@@ -0,0 +1,22 @@
+package ckassWork;
+
+public class ClassWork {
+
+    public static void main(String[] args){
+    boolean ruff = true;
+    boolean wall = true;
+    int w = 12;
+    boolean accept = accept(ruff,wall,w);
+        System.out.println(accept);
+
+
+
+
+}
+
+    static boolean accept ( boolean roof, boolean wall, int w ){
+        return roof && wall && w == 14;
+}
+
+    }
+
